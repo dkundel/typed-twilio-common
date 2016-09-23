cc6e65336267aa1fcaf818e917c28184b45bce56
Dominik Kundel <dominik.kundel@gmail.com>
2016-09-23 17:12:44 +0200

    Initial AccessManager typings

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+import events = require('events');
+
+declare namespace Twilio {
+  export class AccessManager extends events.EventEmitter {
+    constructor(initialToken: string);
+    identity: string | null;
+    token: string;
+    isExpired: boolean;
+    expires: Date | null;
+    updateToken(newToken: string): Promise<this>;
+    on(event: 'error', callback: (err: Error) => void): this;
+    on(event: 'tokenExpired', callback: (manager: AccessManager) => void): this;
+    on(event: 'tokenUpdated', callback: (manager: AccessManager) => void): this;
+  }
+}
